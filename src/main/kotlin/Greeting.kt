6fbb402af8582f23638a399d5102ad7bcd1bbce0
rakhil <rakhil@cisco.com>
2023-04-21 14:37:37 +0530

    Added sample hello_world files

---
+++ src/main/kotlin/Greeting.kt
@@ -0,0 +1,17 @@
+class Greeting (private val greet1: String, private val greet2: String) {
+
+    fun greet1() = greet1
+    fun greet2() = greet2
+}
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+    val greeting = Greeting("Hello", "Fuck off")
+    val greeting2: String = greeting.greet2()
+    val greeting1: String = greeting.greet1()
+    println("Hiiiii...$greeting1")
+    println("Now $greeting2")
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
